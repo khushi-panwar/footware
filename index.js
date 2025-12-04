4193e6ad5945819f1f3c544ebbb7fabc5d61d11d
khushi panwar <125784585+khushi-panwar@users.noreply.github.com>
2025-12-04 17:16:59 +0530

    added the price sort and deal slider functionality

--- index.js
+++ index.js
@@ -13,7 +13,12 @@ let filteredProducts = [];
 const container = document.querySelector("#product-grid")
 const searchInput = document.querySelector("#Item")
 const searchIcon = document.querySelector("#searchIcon");
+const dealSlider = document.querySelector("#deal-card")
+const dealLeft = document.querySelector(".deals-nav.left");
+const dealRight = document.querySelector(".deals-nav.right");
+const priceRange = document.querySelector("#price-range");
 
+console.log(dealSlider);
 
 //  Fake API data fetching
 async function fetchProduct() {
@@ -39,10 +44,14 @@ fetchProduct();
 
 // render product to card
 function renderProduct(products){
-    container.innerHTML= ""
+    container.innerHTML = ""
+    dealSlider.innerHTML = ""
+    
+    
 
     if (products.length === 0) {
         container.innerHTML = "<p> No product found </p>"
+        dealSlider.innerHTML = "<p> No product found </p>"
         return;
     }
     // console.log(products); -------------checking
@@ -51,21 +60,38 @@ function renderProduct(products){
     products.forEach(Item => {
     const div = document.createElement("div");
     div.id="card";
-
-    div.innerHTML = `<i class="ri-heart-3-line"></i>
+    
+        div.innerHTML = `<i class="ri-heart-3-line"></i>
         <img src="${Item.images[0]}"
         alt="">
         <h3>${Item.title}</h3>
         <div id="details">
         <span class="brand">${Item.brand}</span>
         <span class="rating">⭐ ${Item.rating}</span>
-            <span class="">₹${Item.price}</span>
+        <span class="">₹${Item.price}</span>
         </div>
-        <p>$${Item.price}</p>`
+        <p>₹${Item.price}</p>`
             
         container.appendChild(div);
+        
+        const dealcard = document.createElement("div");
+        dealcard.id="deal-item";
+        
+        dealcard.innerHTML = `<img src="${Item.thumbnail}" alt="${Item.title}">
+        <h4>${Item.title}</h4>
+        <p>₹${Item.price}</p>`
+
+        dealSlider.appendChild(dealcard);
     });
 }
+
+dealRight.addEventListener("click", () => {
+    dealSlider.scrollLeft += 300;
+});
+
+dealLeft.addEventListener("click", () => {
+    dealSlider.scrollLeft -= 300;
+});
 
 // search functionality 
 function filters(input) {
@@ -92,4 +118,23 @@ searchInput.addEventListener("keyup", (e) => {
         filters(val);
         renderProduct(filteredProducts);
     }
+})
+
+
+// sorting -filters 
+
+priceRange.addEventListener("change", () => {
+    const option = priceRange.value;
+
+    console.log(option);
+    if (option === "low-high") {
+        filteredProducts.sort((a,b) => a.price - b.price);
+    }
+    else if(option === "high-low"){
+        filteredProducts.sort((a,b) => b.price - a.price)
+    }
+    else{
+    filteredProducts = allProducts;
+    }
+    renderProduct(filteredProducts);
 })
